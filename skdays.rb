66e90663b76b1c770c4b11a2a2f22983852e78f2
Douglas Urner <dlu@canishe.com>
2020-01-11 16:21:10 -0800

    Tidying up and adding description.

--- skdays.rb
+++ skdays.rb
@@ -5,30 +5,32 @@ require 'icalendar'
 
 ### Create iCal ics file with multiple events
 class EventCreator
-  OFF = {}
-
   attr_reader :cal
 
-  def initialize(first, last, file)
+  OFF = {}
+  
+  def initialize(opts)
     @cal = Icalendar::Calendar.new
     # RFC 7986
-    @cal.append_custom_property("NAME", "SKHS Days")
-    @cal.append_custom_property("REFRESH-INTERVAL;VALUE=DURATION", "P1H")
+    @cal.append_custom_property("NAME", opts[:cal_name])
+    @cal.append_custom_property("DESCRIPTION", opts[:desc])
+    @cal.append_custom_property("REFRESH-INTERVAL;VALUE=DURATION", opts[:ttl])
     # Interim values
-    @cal.append_custom_property("X-WR-CALNAME", "SKHS Days")
-    @cal.append_custom_property("X-PUBLISHED-TTL;VALUE=DURATION", "P1H")
-    File.open(file, 'r') do |f|
+    @cal.append_custom_property("X-WR-CALNAME", opts[:cal_name])
+    @cal.append_custom_property("X-WR-CALDESC", opts[:desc])
+    @cal.append_custom_property("X-PUBLISHED-TTL;VALUE=DURATION", opts[:ttl])
+    File.open(opts[:off_file], 'r') do |f|
       f.each_line do |l|
         OFF[Date.parse(l)] = true
       end
     end
-    make_events(first, last)
+    add_events(opts)
   end
 
-  def make_events(first, last)
+  def add_events(opts)
     count = 1
 
-    first.upto(last) do |date|
+    opts[:first_day].upto(opts[:last_day]) do |date|
       if (OFF[date] == true)
         next
       end
@@ -42,22 +44,29 @@ class EventCreator
     end
   end
 
-  def to_ics
-    File.open("docs/skdays.ics", "w") { |f| f.write @cal.to_ical }
+  def to_ics(out_file)
+    File.open(out_file, "w") { |f| f.write @cal.to_ical }
   end
 end
 
 if __FILE__ == $0
   require 'optparse'
 
-  opts = {}
+  # Defaults
+  opts = {
+    cal_name: "SKHS Days",
+    desc: "South Kitsap High School A/B day calendar (with snow days).",
+    out_file: "docs/skdays.ics",
+    ttl: "P1H"
+  }
 
   OptionParser.new do |o|
     o.banner = "Usage: #{$0} [options]"
 
     o.on("-f DATE")     { |v| opts[:first_day] = Date.parse(v) }
-    o.on("-F FILENAME") { |v| opts[:filename]  = v }
+    o.on("-I FILENAME") { |v| opts[:off_file]  = v }
     o.on("-l DATE")     { |v| opts[:last_day]  = Date.parse(v) }
+    o.on("-O FILENAME") { |v| opts[:out_file]  = v }
     o.on("-h")          { puts o; exit }
     o.on("-v")          { |v| opts[:verbose] = v }
 
@@ -65,6 +74,8 @@ if __FILE__ == $0
 
   if (opts[:verbose] == true) then puts opts; end
 
-  calendar = EventCreator.new(opts[:first_day], opts[:last_day], opts[:filename])
-  calendar.to_ics
+  # TODO: validate opts before calling
+
+  calendar = EventCreator.new(opts)
+  calendar.to_ics(opts[:out_file])
 end
