4e79c2308c32c19b816130a4672e0a0ec1882943
Douglas Urner <dlu@canishe.com>
2020-01-14 11:20:58 -0800

    Allow setting reason.

--- snowday.rb
+++ snowday.rb
@@ -10,18 +10,23 @@
 require 'date'
 require 'optparse'
 
-today = Date::today.strftime("%Y-%m-%d")
+today = Date::today.strftime('%Y-%m-%d')
 
-opts = { date: (Date::today.strftime("%Y-%m-%d")), message: "Snow Day" }
+opts = {
+  date: (Date::today.strftime('%Y-%m-%d')),
+  reason: 'X'
+  message: 'Snow Day'
+}
 
 OptionParser.new do |o|
   o.banner = "Usage: #{$0} [options]"
 
-  o.on("-d DATE")     { |v| opts[:date] = Date.parse(v) }
-  o.on("-I FILENAME") { |v| opts[:filename] = v }
-  o.on("-h")          { puts o; exit }
-  o.on("-m MESSAGE")  { |v| opts[:message] = v }
-  o.on("-v")          { |v| opts[:verbose] = v }
+  o.on('-d DATE')     { |v| opts[:date] = Date.parse(v) }
+  o.on('-I FILENAME') { |v| opts[:filename] = v }
+  o.on('-h')          { puts o; exit }
+  o.on('-m MESSAGE')  { |v| opts[:message] = v }
+  o.on('-r REASON')   { |v| opts[:reason] = v }
+  o.on('-v')          { |v| opts[:verbose] = v }
 
 end.parse!
 
@@ -30,4 +35,6 @@ if (opts[:verbose] == true) then
   puts "#{opts[:date]} #{opts[:message]}\n"
 end
 
-File.open(opts[:filename], "a") { |f| f.write "#{opts[:date]}||#{opts[:message]}\n" }
+File.open(opts[:filename], 'a') do |f|
+  f.write "#{opts[:date]}|#{opts[:reason]}|#{opts[:message]}\n"
+end
